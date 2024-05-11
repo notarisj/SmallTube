9bc840ddb5cd46e4142445d5138053a6475ed0d3
notaris <80954329+notarisj@users.noreply.github.com>
2024-05-12 01:54:20 +0300

    Initial Commit

---
+++ SmallTube/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SmallTube
+//
+//  Created by John Notaris on 12/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SmallTube/SmallTubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmallTubeApp.swift
+//  SmallTube
+//
+//  Created by John Notaris on 12/5/24.
+//
+
+import SwiftUI
+
+@main
+struct SmallTubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
