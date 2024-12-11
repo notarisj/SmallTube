38d39113b2d4e544e5459aa7802241d1ae44e8e0
notaris <80954329+notarisj@users.noreply.github.com>
2024-12-11 14:28:21 +0200

    Remove unused YouTubeVideo class

--- SmallTube/YouTubeAPIVideo.swift
+++ SmallTube/YouTubeAPIVideo.swift
@@ -57,6 +57,18 @@ struct YouTubeAPIVideo: Decodable {
     }
 }
 
+extension String {
+    var stringByDecodingHTMLEntities: String {
+        guard let data = self.data(using: .utf8) else { return self }
+        let options: [NSAttributedString.DocumentReadingOptionKey: Any] = [
+            .documentType: NSAttributedString.DocumentType.html,
+            .characterEncoding: String.Encoding.utf8.rawValue
+        ]
+        guard let attributedString = try? NSAttributedString(data: data, options: options, documentAttributes: nil) else { return self }
+        return attributedString.string
+    }
+    }
+
 struct YouTubeResponse: Decodable {
     let items: [YouTubeAPIVideo]
 }
