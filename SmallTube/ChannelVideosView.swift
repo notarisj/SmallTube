9bdb436f5ee8f42ab98ec9bba8b6408a551b6c8f
notaris <80954329+notarisj@users.noreply.github.com>
2024-12-12 12:10:43 +0200

    Update channel view with full description

--- SmallTube/ChannelVideosView.swift
+++ SmallTube/ChannelVideosView.swift
@@ -12,22 +12,48 @@ struct ChannelVideosView: View {
     var channel: YouTubeChannel
 
     var body: some View {
-        List(viewModel.videos) { video in
-            NavigationLink(destination: VideoPlayerView(video: video)) {
-                HStack {
-                    if let url = URL(string: video.thumbnailURL.absoluteString) {
-                        AsyncImage(url: url)
-                            .frame(width: 100, height: 60)
-                            .cornerRadius(8)
-                    }
-                    VStack(alignment: .leading) {
-                        Text(video.title)
-                            .font(.headline)
+        List {
+            // Channel Info as the first entry in the list
+            VStack(alignment: .leading, spacing: 8) {
+                HStack(alignment: .top, spacing: 16) {
+                    AsyncImage(url: channel.thumbnailURL)
+                        .frame(width: 80, height: 80)
+                        .clipShape(Circle())
+
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text(channel.title)
+                            .font(.title2)
+                            .fontWeight(.bold)
                             .lineLimit(2)
-                        Text(video.description)
+                            .truncationMode(.tail)
+
+                        Text(channel.description)
                             .font(.subheadline)
-                            .lineLimit(3)
                             .foregroundColor(.secondary)
+                            .fixedSize(horizontal: false, vertical: true) // Allow multi-line
+                    }
+                }
+                .padding(.vertical, 8)
+            }
+
+            // Videos list
+            ForEach(viewModel.videos) { video in
+                NavigationLink(destination: VideoPlayerView(video: video)) {
+                    HStack {
+                        if let url = URL(string: video.thumbnailURL.absoluteString) {
+                            AsyncImage(url: url)
+                                .frame(width: 100, height: 60)
+                                .cornerRadius(8)
+                        }
+                        VStack(alignment: .leading) {
+                            Text(video.title)
+                                .font(.headline)
+                                .lineLimit(2)
+                            Text(video.description)
+                                .font(.subheadline)
+                                .lineLimit(3)
+                                .foregroundColor(.secondary)
+                        }
                     }
                 }
             }
